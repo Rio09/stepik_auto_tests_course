4b501563ffe47e39f8089e6d6ed69dee35c7bdc9
SiraevRS1 <rinat.siraev@vitechexpert.ae>
2025-05-27 17:14:53 +0500

    Добавил файлы

--- lesson_1_6/lesson6_step10.py
+++ lesson_1_6/lesson6_step10.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 import time
 
 try:
-    link = "https://suninjuly.github.io/registration2.html"
+    link = "http://suninjuly.github.io/registration1.html"
+    #link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
@@ -33,6 +34,6 @@ try:
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
+    time.sleep(3)
     # закрываем браузер после всех манипуляций
     browser.quit()
